3a02e77d827e0875d1244291e52658ae700fe2e5
Jafar1006 <jafaranafa1@gmail.com>
2025-11-18 16:03:57 +0700

    penambahan header

--- src/child.h
+++ src/child.h
@@ -1,1 +1,34 @@
+#ifndef CHILD_H
+#define CHILD_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Definisi Infotype Sales
+struct Mobil {
+    string idMobil;
+    string Merek;
+    string Model
+    int harga;
+    int tahunProduksi;
+};
+
+typedef Sales infotype:
+
+typedef struct elmSales *adrSales;
+
+struct elmSales {
+    infotype info;
+    adrSales next;
+};
+
+struct ListSales {
+    adrSales first;
+};
+
+// Primitif
+void createListSales(ListSales &L);
+
+#endif
 
